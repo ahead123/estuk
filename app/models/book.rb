6ab1687c2fb713b79d358a443c4fc52982793b40
Arthur Head <arthurallenhead@yahoo.com>
2015-02-04 01:20:41 -0800

    added sale model

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,9 +2,10 @@ class Book < ActiveRecord::Base
 	extend FriendlyId
 	friendly_id :name, use: :slugged
 
-	
+
 
 	belongs_to :user
+	has_many :sales
 	has_attached_file :image
 	has_attached_file :resource
 
@@ -18,5 +19,8 @@ class Book < ActiveRecord::Base
 
 	validates :image, attachment_presence: true
 	validates :resource, attachment_presence: true
+
+	validates_numericality_of :price, 
+	greater_than: 49, message: "must be at least 50 cents"
 
 end
